0f7b8d3990d497b754f9eecbeac55b46667a1a43
Tal Levin <80452594+Tal-lev@users.noreply.github.com>
2026-09-04 23:24:31 +0300

    Update ready.lua

--- src/ready.lua
+++ src/ready.lua
@@ -1662,7 +1662,7 @@ modutil.once_loaded.game(function()
 
 
 	-- Crucial, leads to crashes if this doesn't run in the end by another mod
-	if (not rom.mods['NikkelM-Zagreus_Journey']) and (not rom.mods['JarlUlsfark-Tools_of_the_Trade']) then
+	--if (not rom.mods['NikkelM-Zagreus_Journey']) and (not rom.mods['JarlUlsfark-Tools_of_the_Trade']) then
 		SetupRunData()
-	end
+	--end
 end)
